44362f6918c52e79886d4f0decc17bd926611649
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-11-08 18:27:33 -0600

    build: add ev target dir
    Change-Id: Iec754a72d391722c580916b12480ccdc3e423c2d
    
    build: import changes from ev config.mk
    
     That will handle qcom_targets.mk inclusion
    
    Change-Id: I987ad65becc568ea54c5fb6dea5957f96dcd7511

--- core/config.mk
+++ core/config.mk
@@ -96,6 +96,9 @@ SRC_API_DIR := $(TOPDIR)prebuilts/sdk/api
 SRC_SYSTEM_API_DIR := $(TOPDIR)prebuilts/sdk/system-api
 SRC_TEST_API_DIR := $(TOPDIR)prebuilts/sdk/test-api
 
+# Evervolv
+SRC_EVERVOLV_DIR := $(TOPDIR)vendor/ev
+
 # Some specific paths to tools
 SRC_DROIDDOC_DIR := $(TOPDIR)build/make/tools/droiddoc
 
@@ -979,6 +982,9 @@ endif
 endif
 
 endif # PRODUCT_USE_DYNAMIC_PARTITION_SIZE
+
+# Rules for Evervolv targets
+include $(SRC_EVERVOLV_DIR)/build/core/config.mk
 
 # ###############################################################
 # Set up final options.
